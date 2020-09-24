8257e8d1cc3b1096294ae3fababd55f2b630d47e
Peitra Erdi <peitrae@gmail.com>
2020-09-25 05:05:58 +0700

    Add label

--- Problem Solving Approach/charCount.js
+++ Problem Solving Approach/charCount.js
@@ -1,9 +1,12 @@
+// Problem
 // Write a function which takes in a string and returns counts of each character in the string
 
+// Examples
 charCount("aaa") // { a: 4 }
 charCount("hello") // { h:1, e:1, l:2, o:1 }
 charCount("Javascript is awesome") // { j:1, a:3, v:1, s:2, c:1, r:1, i:2, p:1, t:1, w:1, e:2, o:1, m:1 }
 
+// Break it down
 function charCount(str) {
   // make an empty object
   // loop over the string
