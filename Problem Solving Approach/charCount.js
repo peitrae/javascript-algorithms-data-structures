1b81bf40ef763ba047261a9a403cca4ebf306cfe
Peitra Erdi <peitrae@gmail.com>
2020-09-27 20:31:56 +0700

    Check if char is a number/letter

--- Problem Solving Approach/charCount.js
+++ Problem Solving Approach/charCount.js
@@ -22,10 +22,12 @@ function charCount(str) {
   for (let char of str) {
     char = char.toLowerCase();
 
-    if (result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
     }
   }
 
