7f5a6dbe69db7f1eecde9fdcea3c1a60015a7d79
Peitra Erdi <peitrae@gmail.com>
2020-09-27 10:41:48 +0700

    Solve

--- Problem Solving Approach/charCount.js
+++ Problem Solving Approach/charCount.js
@@ -2,17 +2,32 @@
 // Write a function which takes in a string and returns counts of each character in the string
 
 // Examples
-charCount("aaa") // { a: 4 }
-charCount("hello") // { h:1, e:1, l:2, o:1 }
-charCount("Javascript is awesome") // { j:1, a:3, v:1, s:2, c:1, r:1, i:2, p:1, t:1, w:1, e:2, o:1, m:1 }
+charCount("aaa"); // { a: 4 }
+charCount("hello"); // { h:1, e:1, l:2, o:1 }
+charCount("Javascript is awesome"); // { j:1, a:3, v:1, s:2, c:1, r:1, i:2, p:1, t:1, w:1, e:2, o:1, m:1 }
 
 // Break it down
 function charCount(str) {
   // make an empty object
   // loop over the string
-    // if char is a number/letter AND is a key in object, add one to count
-    // if char is a number/letter AND is not a key in object, add it to object and set value to 1
-    // if char is something else, do nothing
-
+  // if char is a number/letter AND is a key in object, add one to count
+  // if char is a number/letter AND is not a key in object, add it to object and set value to 1
+  // if char is something else, do nothing
   // return the object
+}
+
+function charCount(str) {
+  const result = {};
+
+  for (let char of str) {
+    char = char.toLowerCase();
+
+    if (result[char] > 0) {
+      result[char]++;
+    } else {
+      result[char] = 1;
+    }
+  }
+
+  return result;
 }
